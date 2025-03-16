3cef35881b84f1392d2eced18e436c0176ce66d6
SummonRandomDeveloper <jasontran1963@gmail.com>
2025-03-15 20:14:56 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// This is my first js code
+console.log("Hello World?!");
+
+let first_name = "Jason";
+let last_name = null;
+console.log(last_name);
+
+const INTEREST_RATE = 0.3;
+console.log(INTEREST_RATE);
+
+function greet() {
+    console.log("Hello", first_name);
+    console.log("Your interest rate is", INTEREST_RATE);
+}
+
+greet();
